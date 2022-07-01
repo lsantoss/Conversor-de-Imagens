d96ab451310eadf18254f269da44c16fce69c28c
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2022-07-01 15:14:58 -0300

    Iniciando nova padronização de pastas do projeto

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("POC.ImageConverter.Application")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("POC.ImageConverter.Application")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4e7bc2a9-e8c2-4786-85a6-3d67d108e743")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
